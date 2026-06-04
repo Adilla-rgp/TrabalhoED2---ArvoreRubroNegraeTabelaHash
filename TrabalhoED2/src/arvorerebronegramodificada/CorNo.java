d24edc078c8776fe87d3dec2788188b2ec8bc8e8
Adilla-rgp <adillarobertagomespereira@gmail.com>
2026-06-04 14:38:34 -0300

    feat: adicionar enums para controle de cores e status da tabela hash

---
+++ TrabalhoED2/src/arvorerebronegramodificada/CorNo.java
@@ -0,0 +1,10 @@
+package TrabalhoED2.src.arvorerebronegramodificada;
+
+/**
+ * Representação das cores dos nós em uma árvore Rubro-Negra
+ */
+
+public enum CorNo {
+    VERMELHO,
+    PRETO
+}
---
+++ TrabalhoED2/src/tabelahash/StatusEntrada.java
@@ -0,0 +1,7 @@
+package TrabalhoED2.src.tabelahash;
+
+public enum StatusEntrada {
+    VAZIA,  // Posição ainda não está sendo usada
+    OCUPADA, // Posição contém um elemento válido
+    DELETADA  // Posição tinha elemento que foi removido
+}
